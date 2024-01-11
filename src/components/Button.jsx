583bf7e35e5bb58dd2cb8c80d5f2438029dbb7e4
mcscorazza <mcs.corazza@gmail.com>
2024-01-11 14:58:16 -0300

    11-01

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Button.css'
+
+export default function Button(props) {
+  return (
+    <button className={props.typeBtn}>Enviar</button>
+  )
+}
+
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import './Form.css'
 import InputField from './InputField'
+import Button from './Button'
 
 
 export default function Form() {
   return (
-    <div>
-        <form>
+    <div className='form-frame'>
+        <div className='form-title'>Formulario de Cadastro</div>
+        <form className='form-short'>
             <InputField idField="nome" nameField="Nome" typeField="text" />
-            <button>Enviar</button>
+            <InputField idField="email" nameField="E-mail" typeField="email" />
+            <InputField idField="pwd" nameField="Senha" typeField="password" />
+            <InputField idField="pwd2" nameField="Confirmação" typeField="password" />
+            <InputField idField="other" nameField="Outro" typeField="text" />
+            <Button typeBtn="btn-send" />
         </form>
     </div>
   )
