93fd4b618bb3964f49d3dcbad87a80009a6158f3
mcscorazza <mcs.corazza@gmail.com>
2024-01-17 15:51:33 -0300

    mini-pc

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { HomeIcon, UserIcon, ChatBubbleLeftEllipsisIcon, ClipboardIcon, ArrowLongDownIcon, ArrowLeftEndOnRectangleIcon, Bars3Icon } from '@heroicons/react/24/solid';
+import { HomeIcon, UserIcon, ChatBubbleLeftEllipsisIcon, ClipboardIcon, ArrowLeftEndOnRectangleIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import MenuItem from '../partials/MenuItem';
 
 const Sidebar = () => {
+  const [sideBar, setSideBar] = useState(false)
+
   return (
-    <div className="bg-gray-800 text-white w-12 overflow-hidden">
-      <Bars3Icon className="w-8 h-8 mr-2 my-4" />
-      <div className="w-64">
+    <div className={`bg-gray-800 transition-all text-white overflow-hidden ${sideBar ? 'w-56' : 'w-12'}`}>
+      <div className='cursor-pointer border-b border-gray-500 hover:text-orange-200 w-full flex justify-end' onClick={() => setSideBar((prev) => !prev)}> 
+      { sideBar ?
+      <XMarkIcon className="w-8 h-8 mr-3 my-4" />
+      :
+      <Bars3Icon className="w-8 h-8 mr-3 my-4" />
+      }
+      </div>
+      <div className='w-56'>
         <MenuItem toLink="/" nameLink="HOME"><HomeIcon/></MenuItem>
         <MenuItem toLink="/clientes" nameLink="Clientes"><UserIcon/></MenuItem>
         <MenuItem toLink="/pedidos" nameLink="Pedidos"><ClipboardIcon/></MenuItem>
